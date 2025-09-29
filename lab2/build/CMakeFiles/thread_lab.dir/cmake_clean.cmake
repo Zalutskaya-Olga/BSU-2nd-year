dee97b0d3717b99e3a1c0e9841e10d9b9a17af72
Zalutskaya-Olga <299953338z@gmail.com>
2025-09-29 22:53:49 +0300

    Add files via upload

---
+++ lab2/build/CMakeFiles/thread_lab.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/thread_lab.dir/main.cpp.o"
+  "CMakeFiles/thread_lab.dir/main.cpp.o.d"
+  "thread_lab"
+  "thread_lab.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/thread_lab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
