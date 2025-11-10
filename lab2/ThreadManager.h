3f347079ab14b41c4af9cf2ff3f38c73a95ccd50
Zalutskaya-Olga <299953338z@gmail.com>
2025-11-10 23:17:27 +0300

    Update ThreadManager.h

--- lab2/ThreadManager.h
+++ lab2/ThreadManager.h
@@ -1,44 +1,43 @@
 #ifndef THREADMANAGER_H
 #define THREADMANAGER_H
 
-#include <iostream>
-#include <vector>
-#include <pthread.h>
-#include <unistd.h>
-#include <limits>
-#include <stdexcept>
+#include "DataManager.h"
+#include "DataProcessor.h"
+#include "ThreadController.h"
 
 class ThreadManager {
 private:
-    static const int kMinMaxSleepMs = 7;
-    static const int kAverageSleepMs = 12;
-    
-    std::vector<int> data_;
-    int min_value_;
-    int max_value_;
-    double average_value_;
+    DataManager data_manager_;
+    ThreadController thread_controller_;
 
     ThreadManager(const ThreadManager&);
     ThreadManager& operator=(const ThreadManager&);
 
 public:
-    ThreadManager() : min_value_(0), max_value_(0), average_value_(0.0) {}
+    ThreadManager() : thread_controller_(data_manager_) {}
 
-    void readInputData();
-    static void* minMaxThread(void* arg);
-    static void* averageThread(void* arg);
-    void findMinMax();
-    void calculateAverage();
-    void replaceElements();
-    void printArray(const std::string& title) const;
     void execute();
 
-    const std::vector<int>& getData() const { return data_; }
-    int getMinValue() const { return min_value_; }
-    int getMaxValue() const { return max_value_; }
-    double getAverageValue() const { return average_value_; }
+   
+    void findMinMax() { 
+        DataProcessor::findMinMax(data_manager_); 
+    }
     
-    void setData(const std::vector<int>& data) { data_ = data; }
+    void calculateAverage() { 
+        DataProcessor::calculateAverage(data_manager_); 
+    }
+    
+    void replaceElements() { 
+        DataProcessor::replaceElements(data_manager_); 
+    }
+
+    
+    const std::vector<int>& getData() const { return data_manager_.getData(); }
+    int getMinValue() const { return data_manager_.getMinValue(); }
+    int getMaxValue() const { return data_manager_.getMaxValue(); }
+    double getAverageValue() const { return data_manager_.getAverageValue(); }
+    
+    void setData(const std::vector<int>& data) { data_manager_.setData(data); }
 };
 
 #endif
