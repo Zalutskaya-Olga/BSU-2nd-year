6394f8ded15584958ab09d98ebe22274e8b1ffe8
Zalutskaya-Olga <299953338z@gmail.com>
2026-01-01 21:00:25 +0300

    Update conftest.py

--- kolokvium_Zalutskaya/tests/conftest.py
+++ kolokvium_Zalutskaya/tests/conftest.py
@@ -1,5 +1,35 @@
+
 import pytest
 from fastapi.testclient import TestClient
+import sys
+import os
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+os.environ['IGNORE_ENV_FILE'] = 'true'
+
+import app.config
+
+app.config.app_settings = type('TestSettings', (), {
+    'app_name': "Kawaii Todo List 🎀",
+    'debug_mode': True,
+    'api_version': "v1.0.0",
+    'cors_origins': ["*"],
+    'theme': "hello_kitty",
+    'primary_color': "#FF69B4",
+    'secondary_color': "#FFFFFF",
+    'accent_color': "#FFB6C1",
+    'ribbon_color': "#FF1493",
+    'database_url': "sqlite:///./test_todo.db",
+    'redis_host': "localhost",
+    'redis_port': 6379,
+    'redis_db': 0,
+    'redis_password': None,
+    'enable_kitty_sounds': True,
+    'kitty_emoji': "🐱🎀🌸",
+    'default_bow': "pink",
+})()
+
 from app.main import app_instance
 
 
